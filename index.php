e90ef7996068ad4e10f0ea0f4f588f14d7031166
Meth <30674934+methbkts@users.noreply.github.com>
2018-12-13 09:34:23 +0100

    correction routing

--- index.php
+++ index.php
@@ -1,41 +1,64 @@
 <?php
+// Grabs the URI and breaks it apart in case we have querystring stuff
+$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
-require 'vendor/autoload.php';
+// Route it up!
+switch ($request_uri[0]) {
+    // Home page
+    case '/':
+        require 'public/index.php';
+        break;
+    // About page
+    case '/fromages':
+        require 'public/fromages.php';
+		break;
+	// Login page
+	case '/login':
+		require 'public/login.php';
+		break;
+    // Everything else
+    default:
+        header('HTTP/1.0 404 Not Found');
+        require 'public/views/404.php';
+        break;
+}
 
-use App\Router\Router;
+// require 'vendor/autoload.php';
 
-$router = new Router($_GET['url']);
+// use App\Router\Router;
 
-
-
-$router->get('/', function(){
-	// echo "Bienvenue sur la homepage !";
-	include 'public/index.php';
-});
-$router->post('/', function(){
-	include 'public/index.php';
-});
-$router->get('/login', function(){
-	include 'public/login.php';
-});
-$router->get('/admin', function(){
-	include 'public/admin.php';
-});
-$router->post('/admin', function(){
-	include 'public/admin.php';
-});
+// $router = new Router($_GET['url']);
 
 
 
-$router->get('/fromages', function(){
-	// echo "Tous les fromages";
-	include 'public/fromages.php';
-});
-$router->get('/fromages/:id', function($id){
-	echo "Afficher le fromage $id";
-});
-$router->post('/fromages/:id', function($id){
-	echo "Poster le fromage $id";
-});
+// $router->get('/', function(){
+// 	// echo "Bienvenue sur la homepage !";
+// 	include 'public/index.php';
+// });
+// $router->post('/', function(){
+// 	include 'public/index.php';
+// });
+// $router->get('/login', function(){
+// 	include 'public/login.php';
+// });
+// $router->get('/admin', function(){
+// 	include 'public/admin.php';
+// });
+// $router->post('/admin', function(){
+// 	include 'public/admin.php';
+// });
 
-$router->run();
+
+
+// $router->get('/fromages', function(){
+// 	// echo "Tous les fromages";
+// 	include 'public/fromages.php';
+// });
+// $router->get('/fromages/:id', function($id){
+// 	echo "Afficher le fromage $id";
+// });
+// $router->post('/fromages/:id', function($id){
+// 	echo "Poster le fromage $id";
+// });
+
+// $router->run();
