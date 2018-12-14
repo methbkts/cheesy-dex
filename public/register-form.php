14965bf558e4b02348c8fcad40400701292e9619
Meth <30674934+methbkts@users.noreply.github.com>
2018-12-14 09:48:25 +0100

    correction login form & register form

--- public/register-form.php
+++ public/register-form.php
@@ -3,7 +3,7 @@ if (isset($_POST['username'])) {
 	$_SESSION['username'] = $_POST['username'];
 }
 ?>
-<form class="bckgColorForm d-flex flex-col justify-content-center align-self-center">
+<form class="bckgColorForm d-flex flex-col justify-content-center align-self-center" method="post" action="/login">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Email</label>
@@ -14,26 +14,26 @@ if (isset($_POST['username'])) {
       <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
     </div>
   </div>
-  <div class="form-group">
+  <!-- <div class="form-group">
     <label for="inputAddress">Address</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
+  </div> -->
+  <!-- <div class="form-group">
     <label for="inputAddress2">Address 2</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
+  </div> -->
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <!-- <div class="form-group col-md-6">
       <label for="inputCity">City</label>
       <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
+    </div> -->
+    <!-- <div class="form-group col-md-4">
       <label for="inputState">State</label>
       <select id="inputState" class="form-control">
         <option selected>Choose...</option>
         <option>...</option>
       </select>
-	</div>
+	</div> -->
 	<div class="form-group col-lg-12 align-self-center">
   <button type="submit" class="btn btn-primary">Sign in</button>
 </div>
---
+++ public/views/404.php
@@ -0,0 +1,6 @@
+<?php include 'public/layouts/header.php'; ?>
+
+<h1>ERROR 404</h1>
+<h2>Tu vas où gros ?</h2>
+
+<?php include 'public/layouts/footer.php'; ?>
